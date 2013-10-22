90c125175fe717228af2e2eed28c899cf58655e4
Craig Cook <boycook@me.com>
2013-10-22 16:49:10 +0100

    Added basic file and test

---
+++ src/main/scala/org/cccs/sudoku/Solver.scala
@@ -0,0 +1,13 @@
+package main.scala.org.cccs.sudoku
+
+/**
+ * User: boycook
+ * Date: 22/10/2013
+ * Time: 16:37
+ */
+class Solver {
+  val data = Array.ofDim[Int](9, 9)
+  data(1, 1) = (1, 2)
+}
+
+
---
+++ src/test/scala/org/cccs/sudoku/TestSolver.scala
@@ -0,0 +1,10 @@
+package test.scala.org.cccs.sudoku
+
+/**
+ * User: boycook
+ * Date: 22/10/2013
+ * Time: 16:48
+ */
+class TestSolver {
+
+}
