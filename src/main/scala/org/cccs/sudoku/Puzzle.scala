222767b95c24b911ae34745ee61fde9e2ce88197
Craig Cook <boycook@me.com>
2013-10-23 15:04:32 +0100

    Using recursion to print box

--- src/main/scala/org/cccs/sudoku/Puzzle.scala
+++ src/main/scala/org/cccs/sudoku/Puzzle.scala
@@ -12,48 +12,41 @@ class Puzzle {
    */
   val data = Array.ofDim[Int](9, 9)
 
-  val template = "%s %s %s \n" +
-                 "%s %s %s \n" +
-                 "%s %s %s \n"
-
   //TODO: use recursive function to do this
   def set(x: Int, y: Int, v: Array[Int]) = {
-    // val x1 = x * 3
-    // val y1 = y * 3
-    // // data(x1)(y1) = v(0)
-    // // data(x1 + 1)(y1) = v(1)
-    // // data(x1 + 2)(y1) = v(2)
-    // // data(x1)(y1 + 1) = v(3)
-    // // data(x1 + 1)(y1 + 1) = v(4)
-    // // data(x1 + 2)(y1 + 1) = v(5)
-    // // data(x1)(y1 + 2) = v(6)
-    // // data(x1 + 1)(y1 + 2) = v(7)
-    // // data(x1 + 2)(y1 + 2) = v(8)
+    val x1 = x * 3
+    val y1 = y * 3
+    data(x1)(y1) = v(0)
+    data(x1 + 1)(y1) = v(1)
+    data(x1 + 2)(y1) = v(2)
+    data(x1)(y1 + 1) = v(3)
+    data(x1 + 1)(y1 + 1) = v(4)
+    data(x1 + 2)(y1 + 1) = v(5)
+    data(x1)(y1 + 2) = v(6)
+    data(x1 + 1)(y1 + 2) = v(7)
+    data(x1 + 2)(y1 + 2) = v(8)
 
-    def loop(xc: Int, yc: Int, n: Int): Array[Int] = {
-      data(x + xc)(y + yc) = v(n)
-      if (n == 8) v
-      else if (xc == 2) loop(0, yc + 1, n + 1)
-      else loop(xc + 1, yc, n + 1)
-    }
-    loop(0, 0, 0)
+//    def loop(xc: Int, yc: Int, n: Int): Array[Int] = {
+//      data(x + xc)(y + yc) = v(n)
+//      if (n == 8) v
+//      else if (xc == 2) loop(0, yc + 1, n + 1)
+//      else loop(xc + 1, yc, n + 1)
+//    }
+//    loop(0, 0, 0)
   }
 
   def get(x: Int, y: Int): Int = data(x)(y)
 
-  private def boxToString(x: Int, y: Int) = {
-    val x1 = x * 3
-    val y1 = y * 3
-    format(template,
-      data(x1)(y1 + 2), data(x1 + 2)(y1 + 2), data(x1 + 2)(y1 + 2),
-      data(x1)(y1 + 1), data(x1 + 1)(y1 + 1), data(x1 + 2)(y1 + 1),
-      data(x1)(y1),     data(x1 + 1)(y1),     data(x1 + 2)(y1))
+  private def boxToString() = {
+    def loop(x: Int, y: Int, txt: String): String = {
+      if (x == 8 && y == 8) txt + " " + data(x)(y)
+      else if (x == 8) loop(0, y + 1, txt + " " + data(x)(y) + "\n")
+      else loop(x + 1, y, txt + " " + data(x)(y))
+    }
+    loop(0, 0, "")
   }
 
   override def toString = {
-    format(template,
-        boxToString(0, 0), boxToString(0, 1), boxToString(0, 2),
-        boxToString(1, 0), boxToString(1, 1), boxToString(1, 2),
-        boxToString(2, 0), boxToString(2, 1), boxToString(2, 2))
+    boxToString
   }
 }
