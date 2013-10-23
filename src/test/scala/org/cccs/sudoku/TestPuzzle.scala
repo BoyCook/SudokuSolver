595eebdf97b2bbcd94c5608755c36c0002862f4c
Craig Cook <boycook@me.com>
2013-10-23 16:10:57 +0100

    Using trait for test data

--- src/test/scala/org/cccs/sudoku/TestPuzzle.scala
+++ src/test/scala/org/cccs/sudoku/TestPuzzle.scala
@@ -14,28 +14,19 @@ import org.scalatest.junit.JUnitRunner
 class TestPuzzle extends FunSuite {
 
   test("construct puzzle") {
-    val puzzle = new Puzzle
-    puzzle.set(0, 0, Array(1, 2, 3, 4, 1, 6, 7, 8, 9))
-    puzzle.set(1, 0, Array(1, 2, 3, 4, 2, 6, 7, 8, 9))
-    puzzle.set(2, 0, Array(1, 2, 3, 4, 3, 6, 7, 8, 9))
-    puzzle.set(0, 1, Array(1, 2, 3, 4, 4, 6, 7, 8, 9))
-    puzzle.set(1, 1, Array(1, 2, 3, 4, 5, 6, 7, 8, 9))
-    puzzle.set(2, 1, Array(1, 2, 3, 4, 6, 6, 7, 8, 9))
-    puzzle.set(0, 2, Array(1, 2, 3, 4, 7, 6, 7, 8, 9))
-    puzzle.set(1, 2, Array(1, 2, 3, 4, 8, 6, 7, 8, 9))
-    puzzle.set(2, 2, Array(1, 2, 3, 4, 9, 6, 7, 8, 9))
-    println(puzzle)
-
-    assert(puzzle.get(0,0) === 1)
-    assert(puzzle.get(1,1) === 1)
-    assert(puzzle.get(4,1) === 2)
-    assert(puzzle.get(7,1) === 3)
-    assert(puzzle.get(1,4) === 4)
-    assert(puzzle.get(4,4) === 5)
-    assert(puzzle.get(7,4) === 6)
-    assert(puzzle.get(1,7) === 7)
-    assert(puzzle.get(4,7) === 8)
-    assert(puzzle.get(7,7) === 9)
-    assert(puzzle.get(8,8) === 9)
+    new TestData {
+      println(puzzle1)
+      assert(puzzle1.get(0,0) === 1)
+      assert(puzzle1.get(1,1) === 1)
+      assert(puzzle1.get(4,1) === 2)
+      assert(puzzle1.get(7,1) === 3)
+      assert(puzzle1.get(1,4) === 4)
+      assert(puzzle1.get(4,4) === 5)
+      assert(puzzle1.get(7,4) === 6)
+      assert(puzzle1.get(1,7) === 7)
+      assert(puzzle1.get(4,7) === 8)
+      assert(puzzle1.get(7,7) === 9)
+      assert(puzzle1.get(8,8) === 9)
+    }
   } 
 }
--- src/test/scala/org/cccs/sudoku/TestSolver.scala
+++ src/test/scala/org/cccs/sudoku/TestSolver.scala
@@ -14,22 +14,9 @@ import org.cccs.sudoku.Solver
 @RunWith(classOf[JUnitRunner])
 class TestSolver extends FunSuite {
 
-  trait TestData {
-    val puzzle = new Puzzle
-    puzzle.set(0, 0, Array(1, 2, 3, 4, 1, 6, 7, 8, 9))
-    puzzle.set(1, 0, Array(1, 2, 3, 4, 2, 6, 7, 8, 9))
-    puzzle.set(2, 0, Array(1, 2, 3, 4, 3, 6, 7, 8, 9))
-    puzzle.set(0, 1, Array(1, 2, 3, 4, 4, 6, 7, 8, 9))
-    puzzle.set(1, 1, Array(1, 2, 3, 4, 5, 6, 7, 8, 9))
-    puzzle.set(2, 1, Array(1, 2, 3, 4, 6, 6, 7, 8, 9))
-    puzzle.set(0, 2, Array(1, 2, 3, 4, 7, 6, 7, 8, 9))
-    puzzle.set(1, 2, Array(1, 2, 3, 4, 8, 6, 7, 8, 9))
-    puzzle.set(2, 2, Array(1, 2, 3, 4, 9, 6, 7, 8, 9))
-    val solver = new Solver(puzzle)
-  }
-
   test("should work") {
     new TestData {
+      val solver = new Solver(puzzle1)
       assert(1 === 1)
     }
   }
